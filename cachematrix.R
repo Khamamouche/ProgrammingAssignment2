1b6d0bd9571593221bcf571ad737a66430420c8e
Khamamouche <hamamouc@bc.edu>
2017-07-06 15:58:07 -0400

    Week 3 Peer Graded Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The makecachematrix function sets the original matrix, gets the matrix, and then inverses the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-    #testing
+    cache = NULL
+    
+    set = function(z) {
+        x <<- z
+        cache <<- NULL
+    }
+    get = function() x
+    setinverse = function(inverse) inv <<- inverse
+    getinverse = function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
 ## Write a short comment describing this function
+## The cachesolve function outputs the inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+cache = x$getInverse()
+if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+    
+}
+data = x$get()
+inverse = solve(data,...)
+x$setinverse(inverse)
+
+return(inverse)
+
 }
